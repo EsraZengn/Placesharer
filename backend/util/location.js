9c7963ee1356c33c37ae71ebc8bf0642577321ea
es <esrazengn00@gmail.com>
2020-04-15 22:19:33 +0200

    finished building REST API (section 7)

---
+++ backend/util/location.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const HttpError = require('../models/http-error');
+const API_KEY = '';
+
+async function getCoordsForAddress(address) {
+    return {
+    lat: 40.7484474,
+    lng: -73.9871516,
+  };
+
+/*   const response = await axios.get(
+    `http://sth?address=${encodeURIComponent(address)}&key=${API_KEY}`,
+  );
+  const data = await response.data;
+
+  if (!data || data.status === 'ZERO_RESULTS') {
+    throw new HttpError('Could not find location for the specified address', 422);
+  }
+
+  const coordinates = data.results[0].geometry.location;
+  return coordinates; */
+}
+
+module.exports = getCoordsForAddress;
